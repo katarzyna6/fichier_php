321e82bc1bd709648492abc8a45fa2e7f59ca836
unknown <katharina.r@hotmail.fr>
2020-04-07 17:04:35 +0200

    ex.2

--- fichier.php
+++ fichier.php
@@ -18,11 +18,23 @@ echo "<p>La date de dernière mise à jour du site :$str";
 
 
 
-$file2=fopen("compteur.txt", "r+");
-$pages = fgets($file2);
+$file2=fopen("compteur.txt", "c+");
+$line = fgets($file2);
+var_dump($line);
+
+$count = ($line)? $line + 1 : 1;
+var_dump($count);
+
+fseek($file2, 0);
+fwrite($file2, $count);
+fclose($file2);
+
+
+/*$pages = fgets($file2);
+var_dump($page);
 $pages++;
 fseek($file2,0);
 fwrite($file2, $pages);
 fclose($file2);
-echo '<p>Nombre de vues : ' . $pages . '.</p>';
+echo '<p>Nombre de vues : ' . $pages . '.</p>';*/
 
