986dcc03d840ad30e7e1ae0c3c6862a29b76f683
unknown <katharina.r@hotmail.fr>
2020-04-07 11:20:16 +0200

    compter.txt

--- fichier.php
+++ fichier.php
@@ -10,5 +10,15 @@ while($str !== false) {
     var_dump($str);
 }
 
-
 fclose($file);
+
+
+
+$file2=fopen("compteur.txt", "r+");
+$pages = fgets($file2);
+$pages++;
+fseek($file2,0);
+fwrite($file2, $pages);
+fclose($file2);
+echo '<p>Nombre de vues : ' . $pages . '.</p>';
+?>
